27fdd94d87892588536a948c8a8ac56716d0b2c2
Rodes4 <rogelito870704@gmail.com>
2025-04-10 21:50:16 -0400

    subiendo cambios

---
+++ js/scroll.js
@@ -0,0 +1,19 @@
+// Selecciona el botón
+let boton = document.getElementById("boton_arriba");
+
+// Agrega un evento al scroll
+window.onscroll = function() {
+    mostrarBoton();
+  };
+
+  function mostrarBoton() {
+    // Obtén la posición actual del scroll
+    let posicionScroll = document.documentElement.scrollTop;
+  
+    // Muestra el botón si se supera una posición específica (ejemplo: 300px)
+    if (posicionScroll > 1000) {
+      boton.style.display = "block"; // Muestra el botón
+    } else {
+      boton.style.display = "none"; // Oculta el botón
+    }
+  }
